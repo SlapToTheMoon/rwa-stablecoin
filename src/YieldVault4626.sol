f11c853a67a771b2cf50e1c3d97bb5e74ad1c6f2
Huff <jdhu233@uky.edu>
2026-03-10 00:17:09 -0400

    Drill 6: forge fmt

--- src/YieldVault4626.sol
+++ src/YieldVault4626.sol
@@ -6,8 +6,5 @@ import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.so
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract YieldVault4626 is ERC4626 {
-    constructor(IERC20 asset_)
-        ERC20("Yield fUSD", "yfUSD")
-        ERC4626(asset_)
-    {}
+    constructor(IERC20 asset_) ERC20("Yield fUSD", "yfUSD") ERC4626(asset_) {}
 }
